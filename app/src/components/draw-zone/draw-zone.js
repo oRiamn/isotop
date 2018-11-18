961be03321060074c8e1d97e34b0a53613bb9fd3
Romain Durade <durade.romain@gmail.com>
2018-11-18 17:10:21 +0100

    add cursor on drawzone

--- app/src/components/draw-zone/draw-zone.js
+++ app/src/components/draw-zone/draw-zone.js
@@ -11,25 +11,61 @@ export default class DrawZone extends HTMLElement {
 		super();
 		this.innerHTML = html;
 
-		const size=4000;
+		const size=400;
 
 		this.color = new Color();
-		this.cursor = new Ring(new Point(-20000,-20000),3, 2);
+		this.cursor = new Ring(new Point(-20000,-20000),10, 1);
 
-		const c = this.querySelector('.canvas');
-		c.style.width=`${size}px`;
-		c.style.height=`${size}px`;
+		this.canvasContainer = this.querySelector('.canvas');
+		this.canvasContainer.style.width=`${size}px`;
+		this.canvasContainer.style.height=`${size}px`;
 
-		this.canvas = [];
-		this.querySelectorAll('canvas').forEach((element) => {
-			this.canvas.push(new Canvas2d(
+		this.canvasDot = new Canvas2d(
+			this.querySelector('.dot'), {
+				width: size, 
+				height: size
+			}
+		);
+
+		this.layers = [];
+		this.querySelectorAll('.layers canvas').forEach((element) => {
+			this.layers.push(new Canvas2d(
 				element, {
 					width: size, 
 					height: size
 				}
 			));
-		});		
+		});
+		
+		this.selectedLayer = this.layers[0];
+
+		this.setEvents();
 	}
+
+	drawCursor() {
+		// clear dot canvas
+		this.canvasDot.clearAll();
+		this.cursor.draw(this.canvasDot.ctx,{
+			stroke: '#000'
+		});
+	}
+
+	update(e) {
+		// get mouse pos
+		const x = e.clientX - this.offsetLeft,
+			y = e.clientY - this.offsetTop;
+		this.cursor.center.moveTo(x,y);
+		this.drawCursor();
+	}
+
+	setEvents() {
+		const self = this;
+		this.canvasDot.canvas.addEventListener('mousemove', e => {
+			self.update(e);
+		}, false);
+	}
+
+
 }
 
 window.customElements.define('draw-zone', DrawZone);
