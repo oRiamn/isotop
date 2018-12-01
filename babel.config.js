1ded0ab1b4ab5acbb11c407186b8d4f4c5368efe
Romain Durade <durade.romain@gmail.com>
2018-12-02 00:01:34 +0100

    update babel for implement alias

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const resolve = (...paths) => path.resolve(__dirname, ...paths);
+
+const src = './app/src';
+const dist = '/tmp/isotop';
+
+module.exports = {
+	'presets': [
+		[
+			'@babel/preset-env',
+			{
+				'targets': {
+					'esmodules': true
+				}
+			}
+		]
+	],
+	'plugins': [
+		['module-resolver', {
+			'root': [
+				resolve(`${src}`)
+			],
+			'alias': {
+				'@src': resolve(`${src}`),
+				'@components': resolve(`${src}/components`),
+				'@lib': resolve(`${src}/library`)
+			}
+		}]
+	]
+};
