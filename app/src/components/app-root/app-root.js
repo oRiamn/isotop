e2a6cb9f3914fcc4dc3d6b01bc3d482df40f855f
Romain Durade <durade.romain@gmail.com>
2019-01-21 13:10:34 +0100

    remove debug elements

--- app/src/components/app-root/app-root.js
+++ app/src/components/app-root/app-root.js
@@ -20,26 +20,7 @@ export default class AppRoot extends HTMLElement {
 		this.colorpicker = this.querySelector('color-picker');
 		this.drawzone = this.querySelector('draw-zone');
 		this.inputColor = this.querySelector('input#color');
-
-		const rgb = this.querySelector('#rgb'),
-			hex = this.querySelector('#hex'),
-			hsl = this.querySelector('#hsl');
-
-		const colorInput = new CssColor();
-		this.inputColor.oninput = () => {
-			colorInput.fromHEX(this.inputColor.value);
-			this.colorpicker.setCursorColor(colorInput);
-		};
-
 		
-
-		this.colorpicker.onchange = () => {
-			//this.inputColor.value = this.colorpicker.color.toHEX();
-			rgb.textContent = this.colorpicker.color.toRGB() + '\n' + colorInput.toRGB();
-			hsl.textContent = this.colorpicker.color.toHSL() + '\n' + colorInput.toHSL();
-			hex.textContent = this.colorpicker.color.toHEX() + '\n' + colorInput.toHEX();
-		};	
-
 		this.colorpicker.color = color;
 		this.colorpicker.setCursorColor(color);
 		this.drawzone.brush.color = color;
