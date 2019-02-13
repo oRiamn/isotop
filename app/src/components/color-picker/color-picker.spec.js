570c7c0b98c7f84d33c31a2fa8b329a4242e60aa
Romain Durade <durade.romain@gmail.com>
2019-02-13 11:01:21 +0100

    add unit test for color-picker

--- app/src/components/color-picker/color-picker.spec.js
+++ app/src/components/color-picker/color-picker.spec.js
@@ -9,12 +9,19 @@ describe('color-picker componnent', () => {
 	before(async () => {
 		browser = await puppeteer.launch({headless: true});
 		page = await browser.newPage();
+		await page.setViewport({
+			'width': 400,
+			'height': 400
+		});
 	});
 
 	beforeEach(async () => {
-		page.goto('file:///tmp/isotop/html/index.html');
+		page.goto('file:///tmp/isotop/html/test.html');
 		await page.waitForNavigation();
 		await page.evaluate(() => document.body.innerHTML = '<color-picker></color-picker>');
+		await page.evaluate(() => {
+			window.element = document.querySelector('color-picker');
+		});
 	});
 
 	after(async () => {
@@ -37,17 +44,20 @@ describe('color-picker componnent', () => {
 
 	describe('Should setup color', () => {
 
-		it('default size', async () => {
+		it('#setCursorColor: setup color-picker with specific color', async () => {
 			await page.evaluate(() => {
-				var colorpicker = document.querySelector('color-picker'),
-					color = new isotop.color.CssColor();
-				color.fromHEX('#2240b8');
-				colorpicker.setCursorColor(color);
+				var newColor = new isotop.color.CssColor();
+				newColor.fromHEX('#2240b8');
+				element.setCursorColor(newColor);
 			});
 
-			const color = await page.evaluate(() => document.querySelector('color-picker').color.toHEX());
-			
+			const color = await page.evaluate(() => element.color.toHEX());
+			const angle = await page.evaluate(() => element.triangle.angle);
+			const cursorPosition = await page.evaluate(() => element.cursor.center);
+
 			expect(color).to.equal('#2240b8');
+			expect(angle).to.equal(3.9793506945470707);
+			expect(cursorPosition).to.deep.equal({x: 86.58507566962714, y: 102.99322371854794});
 		});
 
 	});
