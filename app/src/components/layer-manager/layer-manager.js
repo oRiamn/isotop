9df8269c9198dd74394ab8299ded77a8016e8e6c
Romain Durade <durade.romain@gmail.com>
2019-02-17 20:05:07 +0100

    WIP: add layer-manager structure

--- app/src/components/layer-manager/layer-manager.js
+++ app/src/components/layer-manager/layer-manager.js
@@ -1,13 +1,33 @@
 require('./layer-manager.scss');
 import html from './layer-manager.pug';
 
-window.customElements.define('layer-manager',  class extends HTMLElement {
+window.customElements.define('layer-manager', class extends HTMLElement {
 	constructor() {
 		super();
 	}
-	
+
 	connectedCallback() {
 		this.innerHTML = html;
+
+		const list = document.querySelectorAll('.list');
+
+		for (let i = 0; i < list.length; i++) {
+			list[i].addEventListener('click', (e) => {
+				e.stopPropagation();
+				if (list[i].classList.contains('active')) {
+					list[i].classList.remove('active');
+				}
+				else if (list[i].parentElement.parentElement.classList.contains('active')) {
+					list[i].classList.add('active');
+				}
+				else {
+					for (let y = 0; y < list.length; y++) {
+						list[y].classList.remove('active');
+					}
+					list[i].classList.add('active');
+				}
+			});
+		}
 	}
 
 	static get observedAttributes() {
