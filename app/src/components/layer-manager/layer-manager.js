75d83383c482d215f69c7c1bd33242f5ff0957e0
Romain Durade <durade.romain@gmail.com>
2019-02-18 13:45:33 +0100

    refacto layer-manager toggle event

--- app/src/components/layer-manager/layer-manager.js
+++ app/src/components/layer-manager/layer-manager.js
@@ -1,38 +1,38 @@
 require('./layer-manager.scss');
 import html from './layer-manager.pug';
+import { TSImportEqualsDeclaration } from 'babel-types';
 
 window.customElements.define('layer-manager', class extends HTMLElement {
 	constructor() {
 		super();
+
+		this.activeLists =  [];
+	}
+
+	toggleList(e){
+		e.stopPropagation();
+		e.preventDefault();
+		const target = e.target.parentElement;
+		if (target.classList.contains('active')) {
+			target.classList.remove('active');
+			this.activeLists.splice( this.activeLists.indexOf(target), 1 );
+		} else {
+			if (!target.parentElement.parentElement.classList.contains('active')) {
+				this.activeLists.forEach((activeElement) => activeElement.classList.remove('active'));
+				this.activeLists=[];
+			}
+			target.classList.add('active');
+			this.activeLists.push(target);
+		}
 	}
 
 	connectedCallback() {
 		this.innerHTML = html;
 
 		const list = this.querySelectorAll('.list > a');
-
-		for (let i = 0; i < list.length; i++) {
-			list[i].addEventListener('click', (e) => {
-				
-				e.stopPropagation();
-				e.preventDefault();
-
-				const target = e.target.parentElement;
-				if (target.classList.contains('active')) {
-					target.classList.remove('active');
-				}
-				else if (target.parentElement.parentElement.classList.contains('active')) {
-					target.classList.add('active');
-				}
-				else {
-					for (let y = 0; y < list.length; y++) {
-						list[y].classList.remove('active');
-					}
-					target.classList.add('active');
-				}
-				
-			});
-		}
+		list.forEach( (listA) => {
+			listA.addEventListener('click', (e) => this.toggleList(e));
+		});
 	}
 
 	static get observedAttributes() {
