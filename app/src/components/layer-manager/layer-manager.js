52a117fbe68c708f8a6df9ffdd313eadc264a9ea
Romain Durade <durade.romain@gmail.com>
2019-03-05 19:55:39 +0100

    WIP: add drag and drop for layer-manager

--- app/src/components/layer-manager/layer-manager.js
+++ app/src/components/layer-manager/layer-manager.js
@@ -10,15 +10,69 @@ window.customElements.define('layer-manager', class extends HTMLElement {
 	constructor() {
 		super();
 		this.activeLists = [];
+		this.dragSrcElement;
 	}
-	
+
+	dragStart(e) {
+		this.dragSrcElement = e.target;
+		this.dragSrcElement.style.opacity = '0.4';
+		e.dataTransfer.effectAllowed = 'move';
+		e.dataTransfer.setData('text/html', this.dragSrcElement.innerHTML);
+	}
+
+	dragEnter(e) {
+		e.target.classList.add('over');
+	}
+
+	dragLeave(e) {
+		e.stopPropagation();
+		e.target.classList.remove('over');
+	}
+
+	dragOver(e) {
+		e.preventDefault();
+		e.dataTransfer.dropEffect = 'move';
+		return false;
+	}
+
+	dragDrop(e) {
+		if (this.dragSrcElement != e.target) {
+			this.dragSrcElement.innerHTML = e.target.innerHTML;
+			e.target.innerHTML = e.dataTransfer.getData('text/html');
+		}
+		return false;
+	}
+
+	dragEnd(e) {
+		var listItens = this.querySelectorAll('layer-group');
+		[].forEach.call(listItens, function (item) {
+			item.classList.remove('over');
+		});
+		e.target.style.opacity = '1';
+	}
+
+	addEventsDragAndDrop(el) {
+		el.addEventListener('dragstart', (e) => this.dragStart(e), false);
+		el.addEventListener('dragenter', (e) => this.dragEnter(e), false);
+		el.addEventListener('dragover', (e) => this.dragOver(e), false);
+		el.addEventListener('dragleave', (e) => this.dragLeave(e), false);
+		el.addEventListener('drop', (e) => this.dragDrop(e), false);
+		el.addEventListener('dragend', (e) => this.dragEnd(e), false);
+	}
+
+
 	connectedCallback() {
 		this.innerHTML = html;
 
 		const listHandles = this.querySelectorAll('layer-group');
-		listHandles.forEach( (handle) => {
+		listHandles.forEach((handle) => {
 			// handle.addEventListener('open', (e) => console.log('open'));
 			// handle.addEventListener('close', (e) => console.log('close'));
+		});
+
+		const itemHandles = this.querySelectorAll('layer-item');
+		itemHandles.forEach((handle) => {
+			this.addEventsDragAndDrop(handle);
 		});
 	}
 
